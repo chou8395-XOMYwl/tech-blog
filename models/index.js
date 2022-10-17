64e1acad222f748c31e3db5d619bf6db2a8ea688
chou8395-XOMYwl <chou8395@gmail.com>
2022-10-17 09:12:41 -0400

    yes

--- models/index.js
+++ models/index.js
@@ -6,8 +6,16 @@ User.hasMany(Post, {
   foreignKey: 'user_id'
 });
 
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+});
+
 Post.belongsTo(User, {
   foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id',
 });
 
 Comment.belongsTo(User, {
@@ -17,14 +25,6 @@ Comment.belongsTo(User, {
 Comment.belongsTo(Post, {
   foreignKey: 'post_id',
   onDelete: 'cascade'
-});
-
-User.hasMany(Comment, {
-  foreignKey: 'user_id',
-});
-
-Post.hasMany(Comment, {
-  foreignKey: 'post_id',
 });
 
 module.exports = { User, Post, Comment };
