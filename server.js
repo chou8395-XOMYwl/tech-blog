d80bafa657f48815579aef656ed3a0d24e2f93b1
chou8395-XOMYwl <chou8395@gmail.com>
2022-10-17 10:30:13 -0400

    yes

--- server.js
+++ server.js
@@ -1,21 +1,16 @@
 const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
-
 const app = express();
 const PORT = process.env.PORT || 3003;
-
-const sequelize = require('./config/connection');
-
 const helpers = require('./utils/helpers')
 const hbs = exphbs.create({helpers});
-
+const sequelize = require('./config/connection');
 const session = require('express-session');
-
 const SequelizeStore = require('connect-session-sequelize')(session.Store)
 
 const sess = {
-  secret: "Supersecretsecret",
+  secret: "secret",
   cookie: {},
   resave: false,
   saveUninitialized: true,
@@ -26,14 +21,11 @@ const sess = {
 
 
 app.use(session(sess))   
-
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use(require('./controllers/'));
 
 sequelize.sync({ force: false }).then(() => {
