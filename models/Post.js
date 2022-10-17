6d4bca07996445c24a4fab91b19a377986b52fae
chou8395-XOMYwl <chou8395@gmail.com>
2022-10-17 09:14:43 -0400

    yes

--- models/Post.js
+++ models/Post.js
@@ -1,32 +1,11 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
-// create our Post model
+
+
 class Post extends Model {
-  static upvote(body, models) {
-    return models.Vote.create({
-      user_id: body.user_id,
-      post_id: body.post_id
-    }).then(() => {
-      return Post.findOne({
-        where: {
-          id: body.post_id
-        },
-        attributes: [
-          'id',
-          'post_content',
-          'title',
-          'created_at',
-          [
-            sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
-            'vote_count'
-          ]
-        ]
-      });
-    });
-  }
 }
 
-// create fields/columns for Post model
+
 Post.init(
   {
     id: {
@@ -52,11 +31,11 @@ Post.init(
     }
   },
   {
-    onDelete: 'cascade',
     sequelize,
     freezeTableName: true,
     underscored: true,
-    modelName: 'post'
+    modelName: 'post',
+    onDelete: 'cascade'
   }
 );
 
